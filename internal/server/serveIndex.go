15f08205be200f4ab2905bd48b0637fe647b9f44
Dottere <balogh.t.1333@gmail.com>
2026-06-09 22:26:50 +0200

    refactor: moved web from internal to path

--- internal/server/serveIndex.go
+++ internal/server/serveIndex.go
@@ -2,8 +2,10 @@ package server
 
 import (
 	"net/http"
+	"path/filepath"
 )
 
 func serveMainPage(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "internal\\web\\src\\index.html")
+
+	http.ServeFile(w, r, filepath.Join("web", "src", "index.html"))
 }
